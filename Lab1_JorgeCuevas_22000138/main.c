1d756946152d6350da4c7ea2609cc4632a10e466
Jorge-Cuevas90003 <jdcm502@gmail.com>
2026-08-04 07:00:51 -0600

    Initialize bit variable in main function

--- Lab1_JorgeCuevas_22000138/main.c
+++ Lab1_JorgeCuevas_22000138/main.c
@@ -5,15 +5,17 @@
 #include "stdio.h"//Para poder utilizart la funcion printf
 
 int main() {
-uint8_t bit; 
+uint8_t bit=1; 
 uint32_t value=0x00000000; //La direccion de memoria
     for (;;) {
 //printf("Hello World\n");
-printf("Ingresa el numero de bit que deseas modificar: \n\r");
-scanf("%u",&bit);
+//printf("Ingresa el numero de bit que deseas modificar: \n\r");
+//scanf("%u",&bit);
 
 //llamar las funciones 
 bitSet(&value,bit);
 bitClear(&value,bit);
+
+
     }
 }
