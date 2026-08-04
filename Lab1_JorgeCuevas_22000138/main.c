b2a70d05715e287e12e9b84cf2ce5204d05ab652
Jorge-Cuevas90003 <jdcm502@gmail.com>
2026-08-04 06:36:47 -0600

    Implement bit manipulation functions and update main loop for user input

--- Lab1_JorgeCuevas_22000138/main.c
+++ Lab1_JorgeCuevas_22000138/main.c
@@ -5,6 +5,15 @@
 #include "stdio.h"//Para poder utilizart la funcion printf
 
 int main() {
+uint8_t bit; 
+uint32_t value=0x00000000; //La direccion de memoria
     for (;;) {
+//printf("Hello World\n");
+printf("Ingresa el numero de bit que deseas modificar: \n\r");
+scanf("%u",&bit);
+
+//llamar las funciones 
+bitSet(&value,bit);
+bitClear(&value,bit);
     }
 }
--- Lab1_JorgeCuevas_22000138/utilities.h
+++ Lab1_JorgeCuevas_22000138/utilities.h
@@ -2,10 +2,57 @@
 #define UTILITIES_H
 
 #include <stdint.h> //uint32_t and uint8_t
+#include "stdio.h"//para poder uitilzia rla funcion printf
+static inline void bitSet(uint32_t *ptr, uint8_t bit){
+if(&ptr!=NULL){
 
-void bitSet(uint32_t *ptr, uint8_t bit);
-void bitClear(uint32_t *ptr, uint8_t bit) ;
-void bitToggle(uint32_t *ptr, uint8_t bit);
-uint8_t stringLength(uint8_t *str);
+if(bit<32){
+
+uint32_t mask=1<<bit;
+*ptr|=mask;
+printf("Bit %d set to 1\n",bit);
+printf("New value: %d\n",*ptr);
+}else{
+
+printf("Invalid bit number ");
+}
+}else{
+printf("Invalid pointer");
+
+}
+
+
+}
+static inline void bitClear(uint32_t *ptr, uint8_t bit) {
+if(&ptr!=NULL){
+
+if(bit<32){
+
+uint32_t mask=1<<bit;
+*ptr&=~mask;
+printf("Bit %d set to 0\n",bit);
+printf("New value: %d\n",*ptr);
+}else{
+
+printf("Invalid bit number ");
+}
+}else{
+printf("Invalid pointer");
+
+}
+
+
+
+}
+static inline void bitToggle(uint32_t *ptr, uint8_t bit){
+
+
+
+}
+static inline uint8_t stringLength(uint8_t *str){
+
+
+}
+
 
 #endif
