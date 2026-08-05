fd02cb8f7b95a26a3c0ba86cdac7793bcd16952e
Jorge-Cuevas90003 <jdcm502@gmail.com>
2026-08-04 20:43:00 -0600

    Refactor bit manipulation functions for clarity and consistency; improve pointer checks and output formatting

--- Lab1_JorgeCuevas_22000138/main.c
+++ Lab1_JorgeCuevas_22000138/main.c
@@ -7,20 +7,25 @@
 int main() {
 uint8_t bit=1; 
 uint32_t value=0x00000000; //La direccion de memoria
-
+uint8_t texto[]="Universidad Galileo";
 //printf("Hello World\n");
 //printf("Ingresa el numero de bit que deseas modificar: \n\r");
 //scanf("%u",&bit);
 
 //llamar las funciones 
-
+printf("Jorge Cuevas     22000138       04/08/2026 \n\r");
 
 bitSet(&value,bit);
+
+bitToggle(&value, bit);
 
 
 bitClear(&value,bit);
 
+stringLength(texto);
     
-
+// Bucle infinito para mantener el procesador en ejecucion
+    for (;;) {
+    }
 
 }
--- Lab1_JorgeCuevas_22000138/utilities.h
+++ Lab1_JorgeCuevas_22000138/utilities.h
@@ -1,82 +1,63 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-#include <stdint.h> //uint32_t and uint8_t
-#include "stdio.h"//para poder uitilzia rla funcion printf
-static inline void bitSet(uint32_t *ptr, uint8_t bit){
-if(&ptr!=NULL){
+#include <stdint.h>
+#include <stdio.h>
 
-if(bit<32){
-
-uint32_t mask=1<<bit;
-*ptr|=mask;
-printf("Bit %d set to 1\r\n",bit);
-printf("New value: %d\r\n",*ptr);
-}else{
-
-printf("Invalid bit number\r\n");
-}
-}else{
-printf("Invalid pointer\r\n");
-
+static inline void bitSet(uint32_t *ptr, uint8_t bit) {
+    if (ptr != NULL) {
+        if (bit < 32) {
+            uint32_t mask = 1UL << bit;
+            *ptr |= mask;
+            printf("Bit %u set to 1\r\n", bit);
+            printf("New value: %u\r\n", *ptr);
+        } else {
+            printf("Invalid bit number\r\n");
+        }
+    } else {
+        printf("Invalid pointer\r\n");
+    }
 }
 
-
-}
 static inline void bitClear(uint32_t *ptr, uint8_t bit) {
-if(&ptr!=NULL){
-
-if(bit<32){
-
-uint32_t mask=1<<bit;
-*ptr&=~mask;
-printf("Bit %d set to 0\r\n",bit);
-printf("New value: %d\r\n",*ptr);
-}else{
-
-printf("Invalid bit number\r\n");
-}
-}else{
-printf("Invalid pointer\r\n");
-
+    if (ptr != NULL) {
+        if (bit < 32) {
+            uint32_t mask = 1UL << bit;
+            *ptr &= ~mask;
+            printf("Bit %u set to 0\r\n", bit);
+            printf("New value: %u\r\n", *ptr);
+        } else {
+            printf("Invalid bit number\r\n");
+        }
+    } else {
+        printf("Invalid pointer\r\n");
+    }
 }
 
-
-
-}
-static inline void bitToggle(uint32_t *ptr, uint8_t bit){
-
-if(&ptr!=NULL){
-
-if(bit<32){
-uint32_t mask=1<<bit;
-if(mask&*ptr==*ptr){
-bitClear(&ptr, bit);
-
-}else{
-bitSet(&ptr, bit);
-
+static inline void bitToggle(uint32_t *ptr, uint8_t bit) {
+    if (ptr != NULL) {
+        if (bit < 32) {
+            uint32_t mask = 1UL << bit;
+            *ptr ^= mask; // Alterna el bit usando XOR
+            printf("Bit %u toggled\r\n", bit);
+            printf("New value: %u\r\n", *ptr);
+        } else {
+            printf("Invalid bit number\r\n");
+        }
+    } else {
+        printf("Invalid pointer\r\n");
+    }
 }
 
+static inline uint8_t stringLength(uint8_t *str) {
+    if (str == NULL) return 0;
 
-
-}else{
-
-printf("Invalid bit number ");
+    uint8_t contador = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        contador++;
+    }
+    printf("The length of the string is: %u\r\n", contador);
+    return contador; // Retorno del resultado
 }
-}else{
-printf("Invalid pointer");
-}
-
-
-}
-static inline uint8_t stringLength(uint8_t *str){
-int contador =0;
-for(int i=0; str[i]!='\0';i++){
-contador++;
-}
-printf("The length of the string is: %d\r\n",contador);
-}
-
 
 #endif
